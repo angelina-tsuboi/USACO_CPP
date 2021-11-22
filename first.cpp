685f12fecfd712e8f2a8cba16a0be3317febc196
Angelina Tsuboi <angelina.t1832@gmail.com>
2021-11-21 23:58:31 -0800

    changed file

---
+++ first.cpp
@@ -0,0 +1,27 @@
+/*
+ID: angelin8
+PROG: ride
+LANG: C++
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int encode(string word){
+    int result = int(word[0]) - 64;
+    for(int i = 1; i < word.length(); i++){
+        result *= int(word[i]) - 64;
+    }
+    return result % 47;
+}
+
+int main(){
+    string comet, group;
+    cin >> comet >> group;
+    if(encode(comet) == encode(group)){
+        cout << "GO" << endl;
+    }else{
+        cout << "STAY" << endl;
+    }
+    return 0;
+}
